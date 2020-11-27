f8b017e7f33bd7903e1bd29c87dc79ae3476e435
Fred Bricon <fbricon@gmail.com>
2020-11-27 16:50:15 +0100

    Add 'Check Login' command so counter appears in Accounts icon
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
 import * as vscode from 'vscode';
 import { RedHatAuthenticationProvider } from './redhat-auth-provider';
 
-
-
 export async function activate(context: vscode.ExtensionContext) {
 	const loginService = await RedHatAuthenticationProvider.build();
 	context.subscriptions.push(vscode.authentication.registerAuthenticationProvider(loginService));
-	
-	const disposable = vscode.commands.registerCommand('redhat.account.login', () => {
-		vscode.authentication.getSession(loginService.id,['managed_service'], {createIfNone: true});
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('redhat.account.status', () => {
+		return vscode.authentication.getSession(loginService.id, ['managed_service']);
+	})
+	);
+	context.subscriptions.push(vscode.commands.registerCommand('redhat.account.login', () => {
+		return vscode.authentication.getSession(loginService.id, ['managed_service'], { createIfNone: true });
+	})
+	);
 }
