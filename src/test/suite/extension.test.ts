16bb24fd3b90f927a0bb7fa5d5ebb7a99e5dd5b3
Fred Bricon <fbricon@gmail.com>
2021-03-12 15:59:50 +0100

    Bogus test change
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -4,7 +4,8 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 
 	test('activation test', async () => {
-		await vscode.extensions.getExtension('redhat.vscode-redhat-account')?.activate()
+		await vscode.extensions.getExtension('redhat.vscode-redhat-account')?.activate();
+		assert.ok(true);
 		// const providers = vscode.authentication.providers.filter( p => p.id === REDHAT_AUTH_SERVICE_ID);
 		// assert.strictEqual(1, providers.length);
 		// assert.strictEqual('Red Hat', providers[0].label);
