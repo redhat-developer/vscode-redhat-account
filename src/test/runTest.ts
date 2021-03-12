b16e947181feab6eef27d1517963be125a3f62d7
Fred Bricon <fbricon@gmail.com>
2021-03-12 14:32:09 +0100

    Add redhat.vscode-commons dependency before running tests
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,9 +1,23 @@
 import * as path from 'path';
+import * as cp from 'child_process';
 
-import { runTests } from 'vscode-test';
+import {
+	downloadAndUnzipVSCode,
+	resolveCliPathFromVSCodeExecutablePath,
+	runTests
+  } from 'vscode-test';
 
 async function main() {
 	try {
+		const vscodeExecutablePath = await downloadAndUnzipVSCode();
+		const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+	
+		// Use cp.spawn / cp.exec for custom setup
+		cp.spawnSync(cliPath, ['--install-extension', 'redhat.vscode-commons'], {
+		  encoding: 'utf-8',
+		  stdio: 'inherit'
+		});
+	
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
@@ -15,6 +29,7 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+		console.error(err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
